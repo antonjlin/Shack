28bbe4cb6982fea25ccd87d752a0268f9a0d4784
antonjlin <antonjlin@gmail.com>
2017-11-05 11:02:53 -0800

    safkjfjakfafa pranav sucks dick

--- app/src/main/java/com/samsung/shack/MainActivity.java
+++ app/src/main/java/com/samsung/shack/MainActivity.java
@@ -13,16 +13,38 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.content.SharedPreferences;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+    static JSONArray parcelNamesJson = new JSONArray();
+    static JSONArray trackingIDsJson = new JSONArray();
+    static JSONArray carrierJson = new JSONArray();
+    Button parcel1 = (Button) findViewById(R.id.parcel1);
+    Button parcel2 = (Button) findViewById(R.id.parcel2);
+    Button parcel3 = (Button) findViewById(R.id.parcel3);
+    Button parcel4 = (Button) findViewById(R.id.parcel4);
+    Button parcel5 = (Button) findViewById(R.id.parcel5);
+
+
+
+
     Context mContext = this;
-    private TextView mTextMessage;
     public static final String database = "database";
     static boolean loggedIn = false;
+
+    public static ArrayList<String> parcelNames =new ArrayList<String>();
+    public static ArrayList<String> trackingIDs =new ArrayList<String>();
+    public static ArrayList<String> carrier = new ArrayList<String>();
+
     //public static boolean loggedIn = false;
 
 
@@ -55,10 +77,10 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences settings = getSharedPreferences(database, 0);
         loggedIn = settings.getBoolean("loggedIn", loggedIn);
         startLogin(loggedIn);
+
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Toast.makeText(mContext, "Hello", Toast.LENGTH_SHORT);
                 switch (item.getItemId()) {
                     case R.id.navigation_home:
                         //mTextMessage.setText(R.string.title_home);
@@ -80,14 +102,91 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 SharedPreferences settings = getSharedPreferences(MainActivity.database, 0);
                 SharedPreferences.Editor editor = settings.edit();
-                editor.putString("pranavisgay", "pranav is very gay");
+                onCreateParcelClicked();
             }
         });
+        parcel1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        parcel2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        parcel3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        parcel3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        parcel3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        for(int x = 0; x<parcelNamesJson.length(); x++) {
+            try {
+                if(parcelNames != null) {
+                    parcelNames.add((String) parcelNamesJson.get(x));
+                    Toast.makeText(this, (String) parcelNamesJson.get(x), Toast.LENGTH_SHORT);
+                }else{
+
+                }
+            } catch(JSONException e){
+                e.printStackTrace();
+            }
+        }
+        for(int x = 0; x<trackingIDsJson.length(); x++) {
+            try {
+                if(trackingIDs != null) {
+                    trackingIDs.add((String) trackingIDsJson.get(x));
+                    Toast.makeText(this, (String) trackingIDsJson.get(x), Toast.LENGTH_SHORT);
+                }else{
+
+                }
+
+            } catch(JSONException e){
+                e.printStackTrace();
+            }
+        }
+        for(int x = 0; x<carrierJson.length(); x++) {
+            try {
+                if(carrier != null) {
+                    carrier.add((String) carrierJson.get(x));
+                    Toast.makeText(this, (String) carrierJson.get(x), Toast.LENGTH_SHORT);
+                } else{
+
+                }
+            } catch(JSONException e){
+                e.printStackTrace();
+            }
+        }
+
+
+
+
     }
         //onProfileClicked();
 
     public void onProfileClicked(){
         Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+    public void onCreateParcelClicked(){
+        Intent intent = new Intent(this, NewParcelActivity.class);
         startActivity(intent);
     }
 
@@ -101,11 +200,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    protected void onStop(){
-        super.onStop();
+    protected void onDestroy(){
+        super.onDestroy();
         SharedPreferences settings = getSharedPreferences(database, 0);
         SharedPreferences.Editor editor = settings.edit();
         editor.putBoolean("loggedIn", loggedIn);
         editor.commit();
+
+        for(String x:parcelNames) {
+            parcelNamesJson.put(x);
+        }
+        for(String x:trackingIDs) {
+            trackingIDsJson.put(x);
+        }
+        for(String x:carrier) {
+            carrierJson.put(x);
+        }
     }
 }
+
---
+++ app/src/main/java/com/samsung/shack/NewParcelActivity.java
@@ -0,0 +1,91 @@
+package com.samsung.shack;
+
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class NewParcelActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> , AdapterView.OnItemSelectedListener {
+
+    Intent switchToMain = new Intent(this, MainActivity.class);
+
+    private static final int REQUEST_READ_CONTACTS = 0;
+
+    private AutoCompleteTextView trackingIdView;
+    private AutoCompleteTextView parcelNameView;
+    private Spinner spinner;
+    String carrier;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newparcel);
+        trackingIdView = (AutoCompleteTextView) findViewById(R.id.trackingID);
+        parcelNameView = (AutoCompleteTextView) findViewById(R.id.parcelName);
+        final String trackingID = trackingIdView.getText().toString();
+        final String parcelName = parcelNameView.getText().toString();
+
+
+        Button confirm = (Button) findViewById(R.id.confirm);
+        confirm.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.parcelNames.add(parcelName);
+                MainActivity.trackingIDs.add(trackingID);
+            }
+        });
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.carrier, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        carrier = parent.getItemAtPosition(pos).toString();
+        MainActivity.carrier.add(carrier);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        Toast.makeText(this, "please choose the carrier",Toast.LENGTH_SHORT);
+
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+}
+
+
